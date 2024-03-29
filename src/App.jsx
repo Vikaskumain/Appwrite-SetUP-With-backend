726a06c37d1e3f2a86ac40862361890ced9f2176
Vikaskumain <121432906+Vikaskumain@users.noreply.github.com>
2024-03-29 19:59:54 +0530

    Appwrite-setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='font-bold text-3xl'>hello this is the redux toolkit libray now im learning </h1>
+    </div>
+  )
+}
+
+export default App
